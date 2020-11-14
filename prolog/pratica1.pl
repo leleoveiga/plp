67d949bff7e700d50ceb98fe95e6e549272fd60d
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2020-11-14 04:38:04 -0300

    pratica1 feita

---
+++ prolog/pratica1.pl
@@ -0,0 +1,15 @@
+quatroDig(N1,N2,N3,N4,"Sim"):- N1 < N3,
+                            N4 =:= (N3 + N2),
+                            ehImpar(N1),
+                            N1 =:= (N2 + 1), !.
+                            
+quatroDig(N1,N2,N3,N4,"Nao").
+
+ehImpar(N):- N mod 2 =\= 0.
+
+main:- read(N1),
+  read(N2),
+  read(N3),
+  read(N4),
+  quatroDig(N1,N2,N3,N4,R),
+  write(R), nl, halt.
